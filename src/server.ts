949d15c7e71a5796fd604c9458e5786eedf7ac63
Willian Monteiro <contato@willianmonteiro.com.br>
2023-11-05 16:02:59 -0300

    chore: env variables using zod schema

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,28 @@
 import fastify from 'fastify';
 import { knex } from './database';
+import { env } from './env';
+// import crypto from 'node:crypto';
 
 const app = fastify();
 
-app.get('/', async () => {
-	const tables = await knex('sqlite_schema').select('*');
-	return tables;
+app.get('/transactions', async () => {
+	const transaction = await knex('transactions')
+		.select('*');
+	return transaction;
 });
 
+// app.get('/transaction', async () => {
+// 	const transaction = await knex('transactions').insert({
+// 		id: crypto.randomUUID(),
+// 		title: 'Test',
+// 		amount: 147.99,
+// 	}).returning('*');
+
+// 	return transaction;
+// });
+
 app
-	.listen({ port: 3333 })
+	.listen({ port: env.HTTP_PORT })
 	.then(() => {
-		console.log('HTTP server running at 3333');
+		console.log(`HTTP server running at ${env.HTTP_PORT}`);
 	});

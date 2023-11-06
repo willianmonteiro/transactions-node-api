61dfc47a4d45b8b05df46ab4950cac08eee84abe
Willian Monteiro <contato@willianmonteiro.com.br>
2023-11-06 14:31:28 -0300

    feat: create endpoints to get transactions: all or by id

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -3,6 +3,24 @@ import { knex } from '../database';
 import { z } from 'zod';
 
 export async function transactionRoutes(app: FastifyInstance) {
+	app.get('/',async (_, response) => {
+		const transactions = await knex('transactions').select();
+		response
+			.send({
+				total: transactions.length,
+				transactions
+			});
+	});
+
+	app.get('/:id',async (request, response) => {
+		const getTransactionParamsSchema = z.object({
+			id: z.string().uuid(),
+		});
+		const { id } = getTransactionParamsSchema.parse(request.params);
+		const transaction = await knex('transactions').where('id', id).first();
+		response.send({ transaction });
+	});
+
 	app.post('/', async (request, response) => {
 		const createTransactionSchema = z.object({
 			title: z.string(),

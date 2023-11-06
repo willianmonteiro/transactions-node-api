4ade9f7e3cedf96affe036a0e9ff51423743f387
Willian Monteiro <contato@willianmonteiro.com.br>
2023-11-06 14:34:52 -0300

    feat: statement endpoint

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -3,7 +3,7 @@ import { knex } from '../database';
 import { z } from 'zod';
 
 export async function transactionRoutes(app: FastifyInstance) {
-	app.get('/',async (_, response) => {
+	app.get('/', async (_, response) => {
 		const transactions = await knex('transactions').select();
 		response
 			.send({
@@ -12,13 +12,19 @@ export async function transactionRoutes(app: FastifyInstance) {
 			});
 	});
 
-	app.get('/:id',async (request, response) => {
+	app.get('/:id', async (request, response) => {
 		const getTransactionParamsSchema = z.object({
 			id: z.string().uuid(),
 		});
 		const { id } = getTransactionParamsSchema.parse(request.params);
 		const transaction = await knex('transactions').where('id', id).first();
 		response.send({ transaction });
+	});
+
+
+	app.get('/statement', async (_, response) => {
+		const statement = await knex('transactions').sum('amount', { as: 'amount' }).first();
+		response.send({ statement });
 	});
 
 	app.post('/', async (request, response) => {

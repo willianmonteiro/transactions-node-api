dc401051c1e12bb6a2c9bfa0bfa9804084a1e4e5
Willian Monteiro <contato@willianmonteiro.com.br>
2023-11-08 13:53:58 -0300

    fix: config to deploy at render

--- src/env/index.ts
+++ src/env/index.ts
@@ -11,7 +11,7 @@ const envSchema = z.object({
 	NODE_ENV: z.enum(['test', 'development', 'staging', 'production']).default('production'),
 	DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
 	DATABASE_URL: z.string(),
-	HTTP_PORT: z.coerce.number().default(3333)
+	PORT: z.coerce.number().default(3333)
 });
 
 const _env = envSchema.safeParse(process.env);
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,10 @@ import { app } from './app';
 import { env } from './env';
 
 app
-	.listen({ port: env.HTTP_PORT })
+	.listen({ 
+		port: env.PORT,
+		host: ('RENDER' in env) ? '0.0.0.0' : 'localhost',
+	})
 	.then(() => {
-		console.log(`HTTP server running at ${env.HTTP_PORT}`);
+		console.log(`HTTP server running at ${env.PORT}`);
 	});
